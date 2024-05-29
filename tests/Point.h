e36d1fc097e69096c997a62cb51420af8d9d9d77
xisseu <164639507+xisseu@users.noreply.github.com>
2024-05-29 20:12:57 +0300

    Update Point.h

--- tests/Point.h
+++ tests/Point.h
@@ -1,10 +1,82 @@
-#ifndef POINT_H
-#define POINT_H
+#include <iostream>
 
-struct Point
+using namespace std;
+class command
 {
-   double x;
-   double y;
+public:
+   virtual void execute()=0;
 };
+class Fan
+{
+public:
+    void SetCircuitTringle()
+    {
+        std::cout<<"SetPower In Tringle toplogy circuit"<<std::endl;
+    }
+  void SetCircuitStar()
+  {
+      std::cout<<"SetPower In star toplogy circuit"<<std::endl;
+  }
+  void ConnectPower()
+  {
+      std::cout<<"Connect Power"<<std::endl;
+  }
+  void DisconnectPower()
+  {
+      std::cout<<"Disconnect Power"<<std::endl;
+  }
+  void Delay(int miliseconds)
+  {
 
-#endif
+       std::cout<<"wait for "<<miliseconds<<" miliseconds"<<std::endl;
+  }
+};
+class TurnOnFanCommand:public command
+{
+    Fan *_fan;
+public:
+    TurnOnFanCommand(Fan*fan)
+    {
+        _fan=fan;
+    }
+
+    void execute() override
+    {
+        _fan->SetCircuitStar();
+        _fan->ConnectPower();
+        _fan->Delay(20000);
+        _fan->SetCircuitTringle();
+    }
+
+};
+class Controller
+{
+    command *_command;
+public:
+    Controller(){}
+    void SetCommand(command* command)
+    {
+        _command=command;
+    }
+    void ButtonClicked()
+    {
+        _command->execute();
+
+    }
+    ~Controller()
+    {
+        delete  _command;
+    }
+};
+class TurnOffFanCommand:public command
+{
+    Fan* _fan;
+public:
+    TurnOffFanCommand(Fan*fan){_fan=fan;}
+
+    void execute() override
+    {
+        _fan->DisconnectPower();
+    }
+
+};
